ddbe23a8c229c3d6ec61d1a07ad9c121dd3ce5b8
Le Duc Long <lelongrvp7@gmail.com>
2023-08-21 15:24:15 +0700

    change link to navlink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -9,15 +9,15 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to="/" className="nav-link">
+            <NavLink to="/" className="nav-link">
               Home
-            </Link>
-            <Link to="/user" className="nav-link">
+            </NavLink>
+            <NavLink to="/user" className="nav-link">
               User
-            </Link>
-            <Link to="/admin" className="nav-link">
+            </NavLink>
+            <NavLink to="/admin" className="nav-link">
               Admin
-            </Link>
+            </NavLink>
           </Nav>
           <Nav>
             <NavDropdown title="Setting" id="basic-nav-dropdown">
