456019db30bd01988687f35d22062a3b35ec344c
Le Duc Long <62542108+lelongrvp@users.noreply.github.com>
2023-08-21 13:59:57 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="app-container">
+      <div>Hello world</div>
+      <button className="btn btn-primary">click here</button>
+    </div>
+  );
+};
+
+export default App;
