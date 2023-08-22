0b5f1f23f0f94049ef1dc46daa2131eb79c767dc
Le Duc Long <lelongrvp7@gmail.com>
2023-08-22 09:07:21 +0700

    add video to homepage

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,7 +1,11 @@
+import videoHomepage from "../../assets/video-homepage.mp4";
+
 const HomePage = (props) => {
   return (
     <div className="homepage-container">
-      <div className="title">Home Page component</div>
+      <video autoPlay muted loop>
+        <source src={videoHomepage} type="video/mp4" />
+      </video>
     </div>
   );
 };
